b344d43fa50a82c8f6dceb84b5cc9f58e9f4d22d
Erin2021 <appleyummy78@gmail.com>
2024-07-15 20:45:34 +0900

    socket4

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,24 +15,75 @@ function addMessage(message){
   ul.appendChild(li);
 }
 
+
+function handleMessageSubmit(event){
+  event.preventDefault();
+  const input = room.querySelector("#msg input");
+  const value = input.value;
+  socket.emit("new_message",input.value,roomName,()=>{
+    addMessage(`You: ${value}`);
+  });
+};
+
+function handleNicknameSubmit(event){
+  event.preventDefault();
+  const input =document.querySelector("#name input");
+  console.log(input);
+  socket.emit("nickname",input.value);
+}
+
+
+
 function showRoom(){
   welcome.hidden=true;
   room.hidden=false;
   const h3=room.querySelector("h3");
   h3.innerText=`Room:${roomName}`
+  const msgForm = room.querySelector("#msg");
+  const nameForm = room.querySelector("#name");
+  msgForm.addEventListener("submit",handleMessageSubmit);
+  nameForm.addEventListener("submit",handleNicknameSubmit);
 }
 
 
 function handleRoomSubmit(event){
   event.preventDefault();
-  const input = form. querySelector("input");
+  const input = form.querySelector("input");
   socket.emit("enter_room",input.value,showRoom);
   roomName=input.value;
   input.value="";
 }
 
+
+
 form.addEventListener("submit",handleRoomSubmit);
 
-socket.on("welcome",()=>{
-  addMessage("someone joined!");
+socket.on("welcome",(user,newCount)=>{
+  const h3=room.querySelector("h3");
+  h3.innerText=`Room:${roomName} (${newCount})`
+
+  addMessage(`${user} joined!`);
 });
+
+
+socket.on("bye",(user,newCount)=>{
+  const h3=room.querySelector("h3");
+  h3.innerText=`Room:${roomName} (${newCount})`;
+
+  addMessage(`${user}  left`);
+});
+
+socket.on("new_message",addMessage);//(msg)=>{addMesssage(msg)}와 같은 뜻임
+
+socket.on("room_change",(rooms)=>{
+  const roomList =welcome.querySelector("ul");
+  roomList.innerHTML ="";
+  if (rooms.length ===0){
+    return;
+  }
+  rooms.forEach(room=>{
+    const li = document.createElement("li");
+    li.innerText =room;
+    roomList.append(li);
+  })
+})
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
 import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui"
 import express, { application } from "express";
 
 const app =express();
@@ -20,18 +21,64 @@ const handleListen =() => console.log(`Listening on http://localhost:3000`);
 
 
 const httpServer = http.createServer(app);
-const wsServer =new Server(httpServer);
+const wsServer =new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true
+  }
+});
+instrument(wsServer, {
+  auth: false,
+  mode: "development",
+});
+
+
+function publicRooms(){
+  const {sockets:{adapter:{sids, rooms}}} = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
+function countRooms(roomName){
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;//roomName 찾은다면 size도 찾아
+}
+
 
 wsServer.on("connection",(socket)=>{
+  socket["nickname"]="Anon";
   socket.onAny((event)=>{
+    // console.log(wsServer.sockets.adapter);
     console.log(`socket event:${event}`);
   })//어느 이벤트에서 작동되는지 콘솔반응함
   socket.on("enter_room",(roomName,done) =>{
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome");
+    socket.to(roomName).emit("welcome",socket.nickname,countRooms(roomName));
+    wsServer.sockets.emit("room_change",publicRooms());
   });
-})
+  socket.on("disconnecting", () => {
+  
+  
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname,countRooms(room)-1)
+    );
+  });
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
+  });
+  socket.on("new_message",(msg,room,done)=>{
+    socket.to(room).emit("new_message",`${socket.nickname}: ${msg}`);
+    done();
+  })
+  socket.on("nickname",nickname=>{
+    socket["nickname"]=nickname;
+  })
+});
 
 
 // const wss = new WebSocket.Server({server});//wss서버생성. 이렇게 해야 http서버와 wss서버 둘다 돌릴 수 있음.인자에 아무것도 안적으면 wss혼자 돌아감
