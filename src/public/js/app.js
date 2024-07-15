a56481730891e6fec83d241e7290abe736afc479
Erin2021 <appleyummy78@gmail.com>
2024-07-15 20:25:18 +0900

    socket 채팅방만들기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,48 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);//backend 연결
+
+function makeMessage(type,payload){
+  const msg={type,payload};
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open",()=>{
   console.log("connected to Server👽")
 })
 
 socket.addEventListener("message",(message)=>{
-  console.log("New message: ",message.data);
+  //console.log("New message: ",message.data);
+  const li = document.createElement('li');
+  li.innerText=message.data;
+  messageList.append(li);
 })
 
 socket.addEventListener("close",()=>{
   console.log("Disconnected to Server👾")
 })
 
-setTimeout(()=>{
-  socket.send("hello from the browser!");
-},10000);//10초후에 backend로 메세지 보내기
+// setTimeout(()=>{
+//   socket.send("hello from the browser!");
+// },10000);//10초후에 backend로 메세지 보내기
+
+function handleSubmit(event){
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message",input.value));
+  const li = document.createElement('li');
+  li.innerText=`You : ${input.value}`;
+  messageList.append(li);
+  input.value="";
+}
+
+function handleNickSubmit(event){
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname",input.value));
+  input.value="";
+}
+
+messageForm.addEventListener("submit",handleSubmit);
+nickForm.addEventListener("submit",handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -4,10 +4,8 @@ import express, { application } from "express";
 
 const app =express();
 
-console.log('helo');
-
-app.set("view engine","pug");
-app.set("views",__dirname +"/views");
+app.set("view engine","pug");//확장자지정
+app.set("views",__dirname +"/views");//폴더경로지정
 
 app.use('/public',express.static(__dirname+"/public"));//유저가 볼수 있는 파일설정
 
@@ -26,15 +24,51 @@ const server = http.createServer(app);//http서버생성.서버에접근할 수 
 
 const wss = new WebSocket.Server({server});//wss서버생성. 이렇게 해야 http서버와 wss서버 둘다 돌릴 수 있음.인자에 아무것도 안적으면 wss혼자 돌아감
 
+
+const sockets =[];
+
 wss.on("connection",(socket)=>{//누가 연결시 발생.socket:연결된 어떤 사람
   //console.log(socket);
-  console.log("connected to Browser👽")
+  sockets.push(socket);
+  socket["nickname"]="Anon";//익명유저를 위해 일단 닉네임에 Anon 넣음
+  console.log("connected to Browser👽");
   socket.on("close",()=>console.log("Disconnected to Browser👾"));
-  socket.on("message",(message)=>{
-    console.log(message.toString());
+  // socket.on("message",(message)=>{
+  //   console.log(message.toString());
+  // });
+  socket.on("message",(msg)=>{
+    // console.log(message.toString());
+    // socket.send(message.toString());
+    const message = JSON.parse(msg); 
+    // if (parsed.type === "new_message"){
+    //   sockets.forEach((aSocket)=>aSocket.send(parsed.payload))
+    // }else if (parsed.type ==="nickname"){
+    //   console.log(parsed.payload);
+    // }
+
+    switch(message.type){
+      case "new_message":
+        sockets.forEach((aSocket)=>aSocket.send(`${socket.nickname}: ${message.payload}`));
+        break;
+      case "nickname":
+        socket["nickname"]=message.payload;//socket은 어쨋뜬 객체라서 추가로 만들어서 넣을 수 있음.여기서 닉네임 업데이트
+        break;
+    }
+    
   });
-  socket.send('helo!')
+  // socket.send('helo!')
   
 });
 
 server.listen(3000, handleListen);
+
+
+//타입나눠서 받아오기 JSON 사용
+// {
+//   type:"message",
+//   payload:"hello!",
+// }
+// {
+//   type:"nickname",
+//   payload:"noco"
+// }
